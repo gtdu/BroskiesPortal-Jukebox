ac80e7155501514d4889dddd9b83a9cb372126fa
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2020-04-22 10:43:20 -0400

    Allow managing playlists

--- delete.php
+++ delete.php
@@ -6,8 +6,15 @@ if ($_SESSION['level'] != 2) {
     die();
 }
 
-$handle = $config['dbo']->prepare('DELETE FROM recommendations WHERE id = ?');
-$handle->bindValue(1, $_GET['id']);
-$handle->execute();
+if (isset($_GET['recommendation'])) {
+    $handle = $config['dbo']->prepare('DELETE FROM recommendations WHERE id = ?');
+    $handle->bindValue(1, $_GET['recommendation']);
+    $handle->execute();
+} else if (isset($_GET['playlist'])) {
+    $handle = $config['dbo']->prepare('DELETE FROM playlists WHERE id = ?; DELETE FROM recommendations WHERE playlistID = ?');
+    $handle->bindValue(1, $_GET['playlist']);
+    $handle->bindValue(2, $_GET['playlist']);
+    $handle->execute();
+}
 
 header("Location: recommendations.php");
--- recommendations.php
+++ recommendations.php
@@ -6,9 +6,19 @@ if ($_SESSION['level'] != 2) {
     die();
 }
 
+if (!empty($_POST)) {
+    $handle = $config['dbo']->prepare('INSERT INTO playlists (name) VALUES (?)');
+    $handle->bindValue(1, $_POST['name']);
+    $handle->execute();
+}
+
 $handle = $config['dbo']->prepare('SELECT recommendations.id, title, artwork, artist, preview_url, name FROM recommendations, playlists WHERE playlistID = playlists.id ORDER BY name, title');
 $handle->execute();
 $data = $handle->fetchAll(\PDO::FETCH_ASSOC);
+
+$handle = $config['dbo']->prepare('SELECT * FROM playlists ORDER BY name');
+$handle->execute();
+$playlists = $handle->fetchAll(\PDO::FETCH_ASSOC);
 
 ?>
 <!doctype html>
@@ -50,11 +60,39 @@ $data = $handle->fetchAll(\PDO::FETCH_ASSOC);
             echo "<td>" . $track['title'] . "</td>";
             echo "<td>" . $track['artist'] . "</td>";
             echo "<td><iframe src='" . $track['preview_url'] . "'></iframe></td>";
-            echo "<td><a href='delete.php?id=" . $track['id'] . "' class='btn btn-danger'>Delete</a></td>";
+            echo "<td><a href='delete.php?recommendation=" . $track['id'] . "' class='btn btn-danger'>Delete</a></td>";
             echo "</tr>";
         }
         ?>
-    </tabl>
+    </table>
+    <hr/>
+    <h2>Playlists</h2>
+    <button class="btn btn-success mb-2" type="button" data-toggle="collapse" data-target="#collapseExample">Create Playlist</button>
+    <div class="collapse" id="collapseExample">
+        <form method="post">
+            <div class="form-group">
+                <label>Playlist Name</label>
+                <input type="text" class="form-control" name="name" placeholder="Tailgates">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+        </form>
+    </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>&nbsp;</th>
+            </tr>
+        </thead>
+        <?php
+        foreach ($playlists as $row) {
+            echo "<tr>";
+            echo "<td>" . $row['name'] . "</td>";
+            echo "<td><a href='delete.php?playlist=" . $row['id'] . "' class='btn btn-danger'>Delete</a></td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
